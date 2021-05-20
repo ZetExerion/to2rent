d31c80d515dd6212957b02834786f9d92065dbdd
Exerion <trenkenshuda@mail.ru>
2021-05-20 13:30:54 +0700

    адаптив

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-$(function(){
+$(function () {
   $('.slider').slick({
     nextArrow: '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next arrow"></button>',
     prevArrow: '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="prev arrow"></button>',
-    fade: true
+    fade: true,
 
-  })
+    responsive: [
+      {
+        breakpoint: 440,
+        settings: {
+          arrows: false
+        }
+      }
+    ]
+  });
 });
 
 
